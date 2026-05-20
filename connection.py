fac0c23a69527c04e62d7433df560e7bf7cc9f04
ByronDev03 <byronjorge.158@gmail.com>
2026-05-20 16:15:28 -0600

    feat: add CRUD

--- connection.py
+++ connection.py
@@ -11,5 +11,47 @@ conexion = mysql.connector.connect(
     database=os.getenv("DB_NAME"),
 )
 
-if conexion.is_connected():
-        print("Conectado a MySQL")
+cursor = conexion.cursor()
+
+
+# INSERT
+sql = "INSERT INTO users (name, email) VALUES (%s, %s)"
+valores = ("veronica", "vero.65@hotmail.com")
+
+cursor.execute(sql, valores)
+
+conexion.commit()
+
+print("Usuario agregado")
+
+
+# SELECT
+cursor.execute("SELECT * FROM users")
+
+users = cursor.fetchall()
+
+for users in users:
+    print(users)
+
+
+# UPDATE
+sql = "UPDATE users SET name = %s WHERE id = %s"
+values = ("Ana", 2)
+
+cursor.execute(sql, values)
+
+conexion.commit()
+
+print("Usuario modificado")
+
+
+# DELETE 
+sql = "DELETE FROM users WHERE id = %s"
+values = (6,)
+
+cursor.execute(sql, values)
+
+conexion.commit()
+
+cursor.close()
+conexion.close()
